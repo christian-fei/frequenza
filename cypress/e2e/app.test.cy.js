06fc40ecaa670a53624dda6cbdece32a24c62244
Christian <crifei93@gmail.com>
2024-09-07 15:21:44 +0000

    improve tests, track and remove entry

--- cypress/e2e/app.test.cy.js
+++ cypress/e2e/app.test.cy.js
@@ -1,8 +1,24 @@
 describe('app', () => {
+  beforeEach(() => {
+    cy.clearLocalStorage()
+    cy.visit('/')
+  })
+
   it('app layout', () => {
-    cy.visit('http://localhost:8080')
     cy.contains('Track')
     cy.contains('Export').click()
     cy.contains('[]') 
+  })
+  it('tracks entry', () => {
+    cy.contains('Track').click()
+    const today = new Date().toISOString().substring(0, 10)
+    cy.contains(today)
+  })
+  it('removes entry', () => {
+    cy.contains('Track').click()
+    cy.on('window:confirm', () => true)
+    const today = new Date().toISOString().substring(0, 10)
+    cy.contains(today).click()
+    cy.get(today).should('not.exist')
   })
 })
