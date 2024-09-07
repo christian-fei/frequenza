0beaf669cc79587daab3be4adfe4d4b3c1d7ab60
Christian <crifei93@gmail.com>
2024-09-07 15:37:23 +0000

    test show entry history

--- cypress/e2e/app.test.cy.js
+++ cypress/e2e/app.test.cy.js
@@ -1,24 +1,37 @@
 describe('app', () => {
   beforeEach(() => {
     cy.clearLocalStorage()
-    cy.visit('/')
   })
-
+  
   it('app layout', () => {
+    cy.visit('/')
     cy.contains('Track')
     cy.contains('Export').click()
     cy.contains('[]') 
   })
   it('tracks entry', () => {
+    cy.visit('/')
     cy.contains('Track').click()
     const today = new Date().toISOString().substring(0, 10)
     cy.contains(today)
   })
   it('removes entry', () => {
+    cy.visit('/')
     cy.contains('Track').click()
     cy.on('window:confirm', () => true)
     const today = new Date().toISOString().substring(0, 10)
-    cy.contains(today).click()
-    cy.get(today).should('not.exist')
+    cy.get('ul > li').click()
+    cy.contains(today).should('not.exist')
+  })
+  it('shows entry history', () => {
+    cy.visit('/', {
+      onBeforeLoad: function (window) {
+        window.localStorage.setItem('history', JSON.stringify([+new Date(), +new Date() - 1000*60*60*24]));
+      }
+    })
+    const today = new Date()
+    cy.contains(today.toISOString().substring(0, 10))
+    const yesterday = new Date(+today - 1000*60*60*24)
+    cy.contains(yesterday.toISOString().substring(0, 10))
   })
 })
