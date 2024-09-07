9dd077076c50a2dbeca8f58d824a84683643d9b8
Christian <crifei93@gmail.com>
2024-09-07 15:21:09 +0000

    cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
+    pageLoadTimeout: 10000,
+    defaultCommandTimeout: 10000,
     supportFile: false,
     baseUrl: 'http://localhost:8080',
   },
