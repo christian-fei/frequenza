77077360735317284c8d6806a28460adc748c917
Christian <crifei93@gmail.com>
2024-09-07 11:53:50 +0000

    basic landing

--- cypress/e2e/app.test.cy.js
+++ cypress/e2e/app.test.cy.js
@@ -2,5 +2,7 @@ describe('app', () => {
   it('app layout', () => {
     cy.visit('http://localhost:8080')
     cy.contains('Track')
+    cy.contains('Export').click()
+    cy.contains('[]') 
   })
 })
